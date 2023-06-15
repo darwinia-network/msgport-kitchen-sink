910630c3bde62283deae2688404daecf36f3be01
Aki Wu <wuminzhe@gmail.com>
2023-06-15 10:53:54 +0800

    update home

--- frontend/components/InstructionsComponent.jsx
+++ frontend/components/InstructionsComponent.jsx
@@ -48,26 +48,26 @@ export default function InstructionsComponent() {
 				</a>
 			</div>
 			<div className={styles.footer}>
-				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-					<img
-						id="badge-button"
-						style={{ width: "240px", height: "53px" }}
-						src="https://static.alchemyapi.io/images/marketing/badgeLight.png"
-						alt="Alchemy Supercharged"
-					/>
-				</a>
 				<div className={styles.icons_container}>
 					<div>
 						<a
-							href="https://github.com/alchemyplatform/create-web3-dapp"
+							href="https://github.com/darwinia-network/darwinia-msgport"
 							target={"_blank"}
 						>
-							Leave a star on Github
+							Darwinia Msgport
 						</a>
 					</div>
 					<div>
 						<a
-							href="https://twitter.com/AlchemyPlatform"
+							href="https://darwinia.network"
+							target={"_blank"}
+						>
+							About Darwinia
+						</a>
+					</div>
+					<div>
+						<a
+							href="https://twitter.com/DarwiniaNetwork"
 							target={"_blank"}
 						>
 							Follow us on Twitter
